26fabdefbeb6d483006126c73bb57c3684676c16
Matheus Penchel <matheus.penchel@gmail.com>
2020-05-26 18:48:45 -0300

    Bad commit: everything went together. But the CRUD is fully implemented, and simple_form and bootstrap were installed 8)

--- livecode/lost_pets/config/routes.rb
+++ livecode/lost_pets/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'pets', to: 'pets#index', as: 'pets'
+  post 'pets', to: 'pets#create'
+
+  get 'pets/new', to: 'pets#new', as: 'new_pet'
+  get 'pets/:id', to: 'pets#show', as: 'pet'
+  delete 'pets/:id', to: 'pets#destroy'
+  patch 'pets/:id', to: 'pets#update'
+
+  get 'pets/:id/edit', to: 'pets#edit', as: 'edit_pet'
+
 end
